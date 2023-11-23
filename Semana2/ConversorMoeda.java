5dc1424e97e4b8639d20bf8088b6585b491e8c48
brenndol <brendo1994@gmail.com>
2023-11-23 19:40:38 -0300

    Convertendo Moeda

---
+++ Semana2/ConversorMoeda.java
@@ -0,0 +1,20 @@
+package conversos;
+import java.util.Scanner;
+
+public class ConversorMoedas {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite a taxa de câmbio: ");
+		double taxaCambio = scanner.nextDouble();
+		
+		System.out.println("Digite a quantidade em dólares: ");
+		double quantidadeDolares = scanner.nextDouble();
+		
+		double valorConvertido = quantidadeDolares * taxaCambio;
+		
+		System.out.println("O valor convertido é: " + valorConvertido);
+		
+		}
+
+}
